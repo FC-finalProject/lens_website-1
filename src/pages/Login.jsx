dfb1fb47bf6b1d3b3dab43b904b5032be664e58d
Dahye <06robin11@gmail.com>
2022-12-01 16:13:45 +0900

    refactor(login):로직을 폼 컴포넌트로 옮기고 react-hook-form으로 리팩토링

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,51 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import LoginForm from '../components/login/LoginForm';
 import LoginOther from '../components/login/LoginOther';
 import Title from '../components/login/Title';
-import Popup from '../components/common/Popup';
-import { useSendLogin } from '../api/loginApi';
-import { useNavigate } from 'react-router';
-import { validation } from '../utils/validation';
-import { useDispatch, useSelector } from 'react-redux';
-import { setLogIn } from '../store/loginInfor';
 
 export default function Login() {
-  const [loginInfor, setLoginInfor] = useState({ loginId: '', password: '' });
-  const [showPopup, setShowPopup] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
-  const { refetch } = useSendLogin(loginInfor);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const isLogin = useSelector((state) => state.isLogin);
-
-  useEffect(() => {
-    if (isLogin) {
-      navigate('/myPage');
-    }
-  }, []);
-
-  const tryToLogin = async (e) => {
-    e.preventDefault();
-    const { loginId, password } = loginInfor;
-    const clientError =
-      validation.idforLogin(loginId) || validation.pwforLogin(password);
-    if (clientError) {
-      setErrorMsg(clientError);
-      setShowPopup(true);
-    } else {
-      const { isError } = await refetch();
-      if (isError) {
-        setErrorMsg('존재하지 않는 아이디이거나 비밀번호가 틀립니다.');
-        setShowPopup(true);
-      } else {
-        dispatch(setLogIn({ login: true }));
-        navigate('/');
-      }
-    }
-  };
-
   return (
     <Container>
       <LoginFormBox>
@@ -53,10 +12,9 @@ export default function Login() {
           subText="나만의 특별함을 위해 LENSSIS에서"
           text="렌즈의 진짜 정보를 찾아보세요."
         />
-        <LoginForm onSubmit={tryToLogin} setLoginInfor={setLoginInfor} />
+        <LoginForm />
         <LoginOther />
       </LoginFormBox>
-      {showPopup && <Popup show={setShowPopup} message={errorMsg} />}
     </Container>
   );
 }
